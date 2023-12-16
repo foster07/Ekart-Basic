9069b5ee04a86d61330deced983f95c3b2e7fa0c
foster07 <9431pk7@gmail.com>
2023-12-16 22:10:25 +0530

    ok

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import CartItem from "../Components/CartItem";
 import { NavLink } from "react-router-dom";
@@ -12,34 +12,44 @@ export default function CartPage() {
   });
   return (
     <div>
-      {cartDataSet.length>0? ( <div className=" flex justify-between  ">
-      <div className="w-full md:w-[55%]">
-        {cartDataSet.map((cartItem) => (
-          <CartItem key={cartItem.id} product={cartItem} />
-        ))}
-      </div>
-      <div className="w-full md:w-[40%] flex flex-col gap-3 h-[80vh] px-9 py-10   justify-center">
-        <p className="text-xl font-bold uppercase text-green-600 ">Your Cart</p>
-        <p className="text-5xl font-bold uppercase text-green-700">Summary</p>
-        <p className="text-xl font-semibold text-slate-700">
-          Total items <span>{cartDataSet.length}</span>
-        </p>
-        <p className="text-2xl font-bold text-slate-700">
-          Total Amount: <span>{`$ ${totalAmount}`}</span>
-        </p>
-        <p><button className=" w-full px-5 py-4 rounded-xl bg-green-300 text-2xl uppercase font-bold hover:bg-green-400">CheckOut Now</button></p>
-      </div>
-    </div>): (
-      <div className="w-full h-[80vh]  flex flex-col gap-5 justify-center items-center">
-        <p className="text-xl font-semibold ">Your Cart Is Empty ! </p>
-        <NavLink 
-        className='px-5 py-4 rounded-xl bg-green-300 text-2xl uppercase font-bold hover:bg-green-400' 
-        to={'/'}>Shop Now </NavLink>
-      </div>
-    )}
-
+      {cartDataSet.length > 0 ? (
+        <div className="flex-col sm:flex lg:flex-row md:flex-row  sm:justify-between  ">
+          <div className="w-full md:w-[55%]">
+            {cartDataSet.map((cartItem) => (
+              <CartItem key={cartItem.id} product={cartItem} />
+            ))}
+          </div>
+          <div className="w-full md:w-[40%] flex flex-col gap-3 h-full sm:h-[80vh] px-9 py-10   justify-center">
+            <p className="text-xl font-bold uppercase text-green-600 ">
+              Your Cart
+            </p>
+            <p className="text-5xl font-bold uppercase text-green-700">
+              Summary
+            </p>
+            <p className="text-xl font-semibold text-slate-700">
+              Total items <span>{cartDataSet.length}</span>
+            </p>
+            <p className="text-2xl font-bold text-slate-700">
+              Total Amount: <span>{`$ ${totalAmount}`}</span>
+            </p>
+            <p>
+              <button className=" w-full px-5 py-4 rounded-xl bg-green-300 text-2xl uppercase font-bold hover:bg-green-400">
+                CheckOut Now
+              </button>
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full h-[80vh]  flex flex-col gap-5 justify-center items-center">
+          <p className="text-xl font-semibold ">Your Cart Is Empty ! </p>
+          <NavLink
+            className="px-5 py-4 rounded-xl bg-green-300 text-2xl uppercase font-bold hover:bg-green-400"
+            to={"/"}
+          >
+            Shop Now{" "}
+          </NavLink>
+        </div>
+      )}
     </div>
-    
-   
   );
 }
